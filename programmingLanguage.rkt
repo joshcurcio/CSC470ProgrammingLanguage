c4c21897a18d4d4772e71cd3fdd7fd7ff5db8278
Josh Curcio <joshua.curcio@cuw.edu>
2015-11-10 17:56:13 -0600

    litman answers

--- programmingLanguage.rkt
+++ programmingLanguage.rkt
@@ -1,6 +1,19 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname |hw process expr|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname programmingLanguage) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+;Global Variables
+(define listOfOperators '(+ - * /))
+
+;generic helper functions
+;returns true if val is found in lst
+(define contains
+  (lambda (val lst)
+    (cond
+      ((null? lst) #f)
+      ((eq? (car lst) val) #t)
+      (else (contains val (cdr lst))))))
+
+;Functions related to the variable environment
 (define empty-env
   (lambda () '()))
 
@@ -31,42 +44,6 @@
             (apply-env var (cdr env))
             resolved))))))
 
-(define lit-exp
-  (lambda (lit) lit))
-
-(define var-exp
-  (lambda (id) id))
-
-(define lambda-exp
-  (lambda (params body)
-    (list 'lambda params body)))
-
-(define app-exp
-  (lambda (rator rands)
-    (append (list rator) rands)))
-
-(define parse-exp
-  (lambda (lcExp)
-    (cond
-      ((number? lcExp) (list 'lit-exp lcExp))
-      ((symbol? lcExp) (list 'var-exp lcExp))
-      ((eq? (car lcExp) 'lambda)
-       (list 'lambda-exp
-             (cadr lcExp)
-             (parse-exp (caddr lcExp))))
-      (else (cons 'app-exp (append (list (parse-exp (car lcExp))) (map parse-exp (cdr lcExp))))))))
-
-(define unparse-exp
-  (lambda (lcStruct)
-    (cond
-      ((eq? (car lcStruct) 'lit-exp) (lit-exp (cadr lcStruct)))
-      ((eq? (car lcStruct) 'var-exp)  (var-exp (cadr lcStruct)))
-      ((eq? (car lcStruct) 'lambda-exp) (lambda-exp
-                                         (list-ref lcStruct 1)
-                                         (unparse-exp (list-ref lcStruct 2))))
-      (else
-       (app-exp (unparse-exp (cadr lcStruct)) (map unparse-exp (caddr lcStruct)))))))
-
 (define extend-env-4-lambda-helper
   (lambda (lovars lovals scope)
     (cond
@@ -82,37 +59,90 @@
      (extend-env-4-lambda-helper lovars lovals (empty-scope))
      env)))
 
+
+;Constructors related to the LCE types
+(define lit-exp
+  (lambda (lit) lit))
+
+(define var-exp
+  (lambda (id) id))
+
+(define lambda-exp
+  (lambda (params body)
+    (list 'lambda params body)))
+
+(define app-exp
+  (lambda (rator rands)
+    (append (list rator) rands)))
+
+
+;return true if the given symbol is a reserved op and false otherwise
+
+;Parser Helper Functions
+;returns true is s is an operator and false otherwise
+(define op?
+  (lambda (s)
+    (contains s listOfOperators)))
+
+;Core Parser Functions
+(define parse-exp
+  (lambda (lcExp)
+    (cond
+      ((number? lcExp) (list 'lit-exp lcExp))
+      ((symbol? lcExp) (if (op? lcExp)
+                           (list 'op-exp lcExp)
+                           (list 'var-exp lcExp)))
+      ((eq? (car lcExp) 'lambda)
+       (list 'lambda-exp
+             (cadr lcExp)
+             (parse-exp (caddr lcExp))))
+      (else (cons 'app-exp (append (list (parse-exp (car lcExp))) (map parse-exp (cdr lcExp))))))))
+
+;evaluates an app expression whose car is an operator
+(define eval-op-exp
+  (lambda (appExp env)
+    appExp))
+    
 (define eval-exp
   (lambda (lce env)
     (cond
       ((eq? (car lce) 'lit-exp) (cadr lce))
-      ((eq? (car lce) 'var-exp) (apply-env (cadr lce) (list env)))
+      ((eq? (car lce) 'var-exp) (apply-env (cadr lce) env))
       ((eq? (car lce) 'lambda-exp) (eval-exp (caddr lce) env))
       (else
-       (if (eq? (list-ref (list-ref lce 1) 0) 'lambda-exp)
+       (cond
+         ((eq? (list-ref (list-ref lce 1) 0) 'lambda-exp)
+           ;first element of app-exp is a lambda
            (eval-exp (list-ref (list-ref lce 1) 2)
                      (extend-env-4-lambda
                       (list-ref (list-ref lce 1) 1)
                       (map (lambda (x)
                              (if (eq? (car x) 'lambda-exp)
                                  x
-                                 (eval-exp x env))) (cddr lce)) env))
-           (let ((resolvedVar (eval-exp (list-ref lce 1) env)))
-             (eval-exp resolvedVar
-                       (extend-env-4-lambda
-                        (list-ref resolvedVar 1)
-                        (map (lambda (x)
-                               (if (eq? (car x) 'lambda-exp)
-                                   x
-                                   (eval-exp x env))) (cddr resolvedVar)) env))))))))
-                     
+                                 (eval-exp x env))) (cddr lce)) env)))
+         ((eq? (list-ref (list-ref lce 1) 0) 'op-exp)
+          ;first element of app-exp is a op-exp
+         (else
+          ;first element of app-exp is a var-exp
+           (let ((theLambda (eval-exp (list-ref lce 1) env))
+                 (theInputs (map (lambda (x)
+                             (if (eq? (car x) 'lambda-exp)
+                                 x
+                                 (eval-exp x env))) (cddr lce))))
+             (eval-exp theLambda (extend-env-4-lambda (list-ref theLambda 1)
+                                                      theInputs
+                                                      env)))))))))
+           
+
 
 (define run-program
   (lambda (lce)
     (eval-exp lce (empty-env))))
 
 
-(define anExp '((lambda (a b) (a b)) (lambda (x) x) 5))
+(define anExp '((lambda (a b c) (a b c)) (lambda (x y) (+ x y)) 5 6))
 ;(define anExp '(lambda (a b) (a b)))
+
 ;Pass the above to apply-env to make sure it comes out
-(run-program (parse-exp anExp))
+(parse-exp anExp)
+;(run-program (parse-exp anExp))
